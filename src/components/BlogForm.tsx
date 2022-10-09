bd4d95adc4a7041360f3db02221f0f7e7b0a5142
Abraham Duno <tresduno@gmail.com>
2022-10-09 02:38:17 -0400

    put request

--- src/components/BlogForm.tsx
+++ src/components/BlogForm.tsx
@@ -55,7 +55,7 @@ const BlogFormComponents: React.FunctionComponent<IBlogFormComponentsProps> = pr
 		if (props.currentlySelected) {
 			return (
 				<div className="eb-form-save-button eb-form-update-button" onClick={() => {
-					alert("not yet")
+					sendUpdateRequestForm()
 				}}>
 					Update
 				</div>
@@ -63,22 +63,14 @@ const BlogFormComponents: React.FunctionComponent<IBlogFormComponentsProps> = pr
 		}
 		return (
 			<div className="eb-form-save-button  " onClick={() => {
-				sendRequestForm()
+				sendCreateRequestForm()
 			}}>
 				Save
 			</div>
 		);
 	}
 
-	const sendRequestForm = async () => {
-		// const _getArticlesResults = await axios.get("https://servicepad-post-api.herokuapp.com/articles/")
-
-		// const _getArticlesResults = await axios({url:"https://randomuser.me/api", method: 'get',})
-		// console.log("_getArticlesResults",_getArticlesResults.data.data.splice(0,4))
-		// const randomName = _getArticlesResults.data.results[0].name
-		// set_latestArticles([{name: `${randomName.first} ${randomName.last}`}]);
-
-		
+	const sendCreateRequestForm = async () => {
 		try {
 			const _data = {
 				author: _currentArticle.author,
@@ -98,10 +90,30 @@ const BlogFormComponents: React.FunctionComponent<IBlogFormComponentsProps> = pr
 					alert("success");
 				}
 			}
-			// const last4Articles = _getArticlesResults.data.data.splice(0,4);
-			// console.log("last4Articles",last4Articles);
-			// set_latestArt;
-			// icles(last4Articles);
+		} catch (error) {
+			alert("error");
+		}
+	}
+	const sendUpdateRequestForm = async () => {
+		try {
+			const _data = {
+				author: _currentArticle.author,
+				title: _currentArticle.title,
+				content: _currentArticle.content,
+			}
+			const promptResult = prompt("Type confirm to update article")
+			if (promptResult == "confirm")
+			{
+				const _postArticlesResult = await axios({url:`https://servicepad-post-api.herokuapp.com/articles/${_currentArticle.id}`, method: 'put',
+					data: _data
+				})
+				console.log("_postArticlesResult", _postArticlesResult)
+				
+				// if (_postArticlesResult.message == "article inserted")
+				{
+					alert("success");
+				}
+			}
 		} catch (error) {
 			alert("error");
 		}
