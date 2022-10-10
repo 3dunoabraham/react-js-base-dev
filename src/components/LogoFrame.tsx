570def10b1f5d360d2a7a43199fa5a9b0d658651
Abraham Duno <tresduno@gmail.com>
2022-10-10 19:20:42 -0400

    menu cta fix

--- src/components/LogoFrame.tsx
+++ src/components/LogoFrame.tsx
@@ -11,10 +11,10 @@ const LogoFrameComponents: React.FunctionComponent<ILogoFrameComponentsProps> = 
 	const _navigate = useNavigate();
 
 	return (
-		<div className="flex-center px-8 tx-lg eb-tx-main tx-bold clickable" onClick={() => { window.scrollTo(0,0);_navigate("/") }}>
+		<div className={(props.responsivemode ? "px-4" : "px-8")+" flex-center  tx-lg eb-tx-main tx-bold clickable"} onClick={() => { window.scrollTo(0,0);_navigate("/") }}>
 			<div className="eb-logo-bar"></div>
 			<div className="eb-logo-bar opacity-50"></div>
-			<div className="eb-logo-bar mr-1 opacity-10"></div>
+			<div className="eb-logo-bar mr-2 opacity-10"></div>
 			<img className={props.responsivemode ? props.responsivemode : ""} style={props.darkmode ? {filter:"brightness(99)"} : {}} src={logo} alt="nologo" width="108" height="20" />
 		</div>
 	);
