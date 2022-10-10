39b4497db00a02e0db36126e5b5edd44f766c735
Abraham Duno <tresduno@gmail.com>
2022-10-10 09:25:05 -0400

    scroll top main logo click

--- src/components/LogoFrame.tsx
+++ src/components/LogoFrame.tsx
@@ -11,7 +11,7 @@ const LogoFrameComponents: React.FunctionComponent<ILogoFrameComponentsProps> = 
 	const _navigate = useNavigate();
 
 	return (
-		<div className="flex-center px-8 tx-lg eb-tx-main tx-bold clickable" onClick={() => { _navigate("/") }}>
+		<div className="flex-center px-8 tx-lg eb-tx-main tx-bold clickable" onClick={() => { window.scrollTo(0,0);_navigate("/") }}>
 			<div className="eb-logo-bar"></div>
 			<div className="eb-logo-bar opacity-50"></div>
 			<div className="eb-logo-bar mr-1 opacity-10"></div>
