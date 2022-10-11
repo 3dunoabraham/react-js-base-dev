11bf2389e9a5d47360a6ae768eecb5ba7a4f0039
Abraham Duno <tresduno@gmail.com>
2022-10-11 09:52:03 -0400

    pagination

--- src/components/BlogPageNavigation.tsx
+++ src/components/BlogPageNavigation.tsx
@@ -21,15 +21,15 @@ const BlogPageNavigationComponents: React.FunctionComponent<IBlogPageNavigationC
 	if (props.pagination.maxPage > 1) {
 		return (
 			<div className="w-100">
-				<div className="eb-articles-table-paginate flex-between flex-column flex-sm_x-row mt-8  eb-tx-grey">
-					<div onClick={() => { props.prev() }} className="pa-5 eb-articles-table-prev tx-bold-4 flex eb-border-t flex-1 clickable opac-hover--50" >
-						<div className="pr-2">←</div>
-						<div className="show-md_x">Previous</div>
+				<div className="eb-articles-table-paginate flex-between flex-row mt-8  eb-tx-grey">
+					<div onClick={() => { props.prev() }} className=" eb-articles-table-prev tx-bold-4 flex eb-border-t flex-1 clickable opac-hover--50" >
+						<div className="py-5  px-2">←</div>
+						<div className="show-md_x pa-5">Previous</div>
 
 					</div>
 					<div className="eb-articles-table-pages flex  tx-bold-4" >
 						{isLongList  && isOverLongIndex &&
-							<div className={(1 != props.pagination.index ? "eb-border-t" : "eb-border-t-primary")+" px-4 py-5 clickable opac-hover--50 eb-border-t-primary"}
+							<div className={(1 != props.pagination.index ? "eb-border-t" : "eb-border-t-primary")+" py-5 px-3 clickable opac-hover--50 eb-border-t-primary"}
 								onClick={() => {props.setPage(1) }}
 							>
 								1
@@ -37,7 +37,7 @@ const BlogPageNavigationComponents: React.FunctionComponent<IBlogPageNavigationC
 						}
 						{isLongList  && isOverLongIndex &&
 							<div className="pa-5 px-0 eb-border-t">
-								. . .
+								...
 							</div>
 						}
 						
@@ -46,7 +46,7 @@ const BlogPageNavigationComponents: React.FunctionComponent<IBlogPageNavigationC
 								.map((i,index) => {
 									// if (index == 0) return <></>
 									if (!isLongList && index+1 == props.pagination.maxPage) return <div key={index+1}></div>
-									return <div className={(index+1 != props.pagination.index ? "eb-border-t" : "eb-border-t-primary")+" pa-5 px-4 clickable opac-hover--50 "}
+									return <div className={(index+1 != props.pagination.index ? "eb-border-t" : "eb-border-t-primary")+" pa-5 px-3 clickable opac-hover--50 "}
 										key={index+1}
 										onClick={() => {
 											props.setPage(index+1)
@@ -60,7 +60,7 @@ const BlogPageNavigationComponents: React.FunctionComponent<IBlogPageNavigationC
 									.map((i,index) => {
 										if (index == 0) return <div key={props.pagination.index+index-2} ></div>
 										if (props.pagination.index+index-2 == props.pagination.maxPage) return <div key={props.pagination.index+index-2} ></div>
-										return <div className={(props.pagination.index+index-2 != props.pagination.index ? "eb-border-t" : "eb-border-t-primary")+" pa-5 px-4 clickable opac-hover--50 "}
+										return <div className={(props.pagination.index+index-2 != props.pagination.index ? "eb-border-t" : "eb-border-t-primary")+" pa-5 px-3 clickable opac-hover--50 "}
 											key={props.pagination.index+index-2}
 											onClick={() => {
 												props.setPage(props.pagination.index+index-2)
@@ -71,15 +71,15 @@ const BlogPageNavigationComponents: React.FunctionComponent<IBlogPageNavigationC
 							}
 						</div>
 						{isLongerList && !isAtLastPage && !isAtSecondToLastPage && <div className="pa-5 px-0 eb-border-t">
-							. . .
+							...
 						</div>}
-						<div onClick={() => {props.setPage(props.pagination.maxPage) }} className={(props.pagination.maxPage != props.pagination.index ? "eb-border-t" : "eb-border-t-primary")+" pa-5 px-4 clickable opac-hover--50 "} >
+						<div onClick={() => {props.setPage(props.pagination.maxPage) }} className={(props.pagination.maxPage != props.pagination.index ? "eb-border-t" : "eb-border-t-primary")+" pa-5 px-3 clickable opac-hover--50 "} >
 							{props.pagination.maxPage}
 						</div>
 					</div>
-					<div onClick={() => { props.next() }} className="pa-5 eb-articles-table-next tx-bold-4 flex flex-1 eb-border-t flex flex-justify-end clickable opac-hover--50" >
-						<div className="show-md_x">Next</div>
-						<div className="pl-2">→</div>
+					<div onClick={() => { props.next() }} className=" eb-articles-table-next tx-bold-4 flex flex-1 eb-border-t flex flex-justify-end clickable opac-hover--50" >
+						<div className="show-md_x pa-5">Next</div>
+						<div className=" py-5 px-2">→</div>
 					</div>
 				</div>
 			</div>
