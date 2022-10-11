fe0bc572e6c18ad5bbf9eea0affe986e50912411
Abraham Duno <tresduno@gmail.com>
2022-10-11 02:22:22 -0400

    styles fix upgrade

--- src/components/ArticleTableHeader.tsx
+++ src/components/ArticleTableHeader.tsx
@@ -9,24 +9,6 @@ const ArticleTableHeaderComponents: React.FunctionComponent<IArticleTableHeaderC
 	return (
 		<div className="eb-article-row w-100 eb-border-b-grey">
 			<div className="flex flex-between flex-justify-start flex-align-end tx-sm" >
-				{false && <><div className="w-12 ma-0 pa-4 py-3 tx-bold-300 show-md_x">
-					AUTHOR NAME
-				</div>
-				<div className="w-20 ma-0 pa-4 py-3 tx-bold-300 show-md_x" >
-					TITLE
-				</div>
-				<div className="flex-1 ma-0 pa-4 py-3 tx-bold-300">
-					<div className="show-xs_md tx-bold mb-3">AUTHOR</div>
-					<div className="show-xs_md mb-3">TITLE</div>
-					<div>CONTENT</div>
-				</div>
-				<div className="w-10 ma-0 pa-4 py-3 tx-bold-300 show-md_x" >
-					DATE
-				</div>
-				<div className="eb-tx-primary ma-0 noborder clickable pa-4 py-3 opacity-hover--50 tx-bold-400">
-					<div className="invisible">Edit</div>
-				</div>
-				</>}
 				<div className="w-12  ma-0 px-4 pa-3 tx-bold-200   show-md_x">
 					AUTHOR NAME
 				</div>
@@ -34,6 +16,8 @@ const ArticleTableHeaderComponents: React.FunctionComponent<IArticleTableHeaderC
 					TITLE
 				</div>
 				<div className="flex-1 ma-0 px-4 pa-3 tx-bold-200 ">
+					<div className="show-xs_md  tx-bold mb-3">AUTHOR</div>
+					<div className="show-xs_md tx-lg mb-3">TITLE</div>
 					<div>CONTENT</div>
 				</div>
 				<div className="w-10 eb-article-highlight ma-0 px-4 py-3 tx-bold-200 tx-start show-md_x">
